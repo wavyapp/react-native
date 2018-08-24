2ae4d6b78d5098c307b6c91fe56adaf8e22a2127
Abel Chalier <abel@wavy.fr>
2018-08-25 00:29:48 +0200

    Handle update of  of  in iOS #18587

--- Libraries/Text/TextInput/RCTBaseTextInputView.m
+++ Libraries/Text/TextInput/RCTBaseTextInputView.m
@@ -430,12 +430,25 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithFrame:(CGRect)frame)
 
 - (void)didSetProps:(NSArray<NSString *> *)changedProps
 {
+  [self shouldHandleSecureTextEntryChange:changedProps];
   if ([changedProps containsObject:@"inputAccessoryViewID"] && self.inputAccessoryViewID) {
     [self setCustomInputAccessoryViewWithNativeID:self.inputAccessoryViewID];
   } else if (!self.inputAccessoryViewID) {
     [self setDefaultInputAccessoryView];
   }
 }
+
+- (void)shouldHandleSecureTextEntryChange:(NSArray<NSString *> *)changedProps
+{
+  BOOL didSecureTextEntryUpdate = [changedProps containsObject: @"secureTextEntry"];
+  if (didSecureTextEntryUpdate) {
+    NSAttributedString *originalText = self.backedTextInputView.attributedText;
+    
+    self.backedTextInputView.attributedText = nil;
+    self.backedTextInputView.attributedText = originalText;
+  }
+}
+
 
 - (void)setCustomInputAccessoryViewWithNativeID:(NSString *)nativeID
 {
